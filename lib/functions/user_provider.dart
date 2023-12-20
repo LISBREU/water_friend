32cf1677d988f53abe9d6d06a2d9d45e442045d7
Ilnur Shabanov <ilnurshabanov79@gmail.com>
2023-12-20 18:02:56 +0300

    add progress bar and notification if day norm done

--- lib/functions/user_provider.dart
+++ lib/functions/user_provider.dart
@@ -5,11 +5,13 @@ class UserProvider with ChangeNotifier {
   int _age = 0;
   double _weight = 0.0;
   double _climate = 1.0;
+  double _waterIntake = 0.0;
 
   int get selectedGender => _selectedGender;
   int get age => _age;
   double get weight => _weight;
   double get climate => _climate;
+  double get waterIntake => _waterIntake;
 
   set selectedGender(int value) {
     _selectedGender = value;
@@ -34,5 +36,15 @@ class UserProvider with ChangeNotifier {
   double calculateWaterIntake() {
     double baseWaterIntake = weight * climate * selectedGender;
     return baseWaterIntake;
+  }
+
+  setWaterIntake(double value) {
+    _waterIntake = value;
+    notifyListeners();
+  }
+
+  void addWaterIntake(double value) {
+    _waterIntake += value;
+    notifyListeners();
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart'; // Импортируйте пакет
 import 'package:provider/provider.dart';
 import '../functions/user_provider.dart';
 
@@ -15,8 +16,38 @@ class Home extends StatelessWidget {
             // Используйте Consumer для получения и использования данных
             Consumer<UserProvider>(
               builder: (context, userProvider, _) {
-                double waterIntake = userProvider.calculateWaterIntake();
-                return Text('Количество воды: $waterIntake мл');
+                double waterIntake = userProvider.waterIntake;
+                double maxWaterIntake = userProvider.calculateWaterIntake();
+
+                if (waterIntake >= maxWaterIntake) {
+                  // Выводим уведомление "Норма дня выполнена"
+                  Fluttertoast.showToast(
+                    msg: "Норма дня выполнена!",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.green,
+                    textColor: Colors.white,
+                    fontSize: 16.0,
+                  );
+                }
+
+                return Column(
+                  children: [
+                    Text('Количество воды: $maxWaterIntake мл'),
+                    LinearProgressIndicator(
+                      value: waterIntake / maxWaterIntake,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Используйте метод провайдера для добавления воды
+                        userProvider.addWaterIntake(250.0);
+                      },
+                      child: Text('Добавить 250 мл'),
+                    ),
+                    Text('Количество воды: $waterIntake мл'),
+                  ],
+                );
               },
             ),
 
