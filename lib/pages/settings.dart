329a2720a81fce82acb5a8a14f737f35ae0ee8c3
Ilnur Shabanov <ilnurshabanov79@gmail.com>
2023-12-29 00:15:56 +0300

    add basic push notification

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import '../functions/notifications.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  void initState() {
+    super.initState();
+    LocalNotification.initilaze(flutterLocalNotificationsPlugin);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Settings',
-        style: TextStyle(
-          color: Colors.blue,
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(20)),
+          width: 180,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: () {
+              LocalNotification.showBigTextNotification(
+                  title: 'Water_Friend',
+                  body: 'Пора выпить воды !',
+                  flutterLocalNotificationsPlugin:
+                      flutterLocalNotificationsPlugin);
+            },
+            child: const Text(
+              "send",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
