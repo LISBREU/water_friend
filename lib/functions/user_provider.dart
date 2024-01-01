dd644e9f1cc54b58bbec32ccd1dd34248aca5588
Ilnur Shabanov <ilnurshabanov79@gmail.com>
2024-01-01 22:09:16 +0300

    add memory for water_intake and users data

--- lib/functions/user_provider.dart
+++ lib/functions/user_provider.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProvider with ChangeNotifier {
-  int _selectedGender = 0; // 0 по умолчанию
+  int _selectedGender = 0;
   int _age = 0;
   double _weight = 0.0;
   double _climate = 1.0;
@@ -41,10 +42,31 @@ class UserProvider with ChangeNotifier {
   setWaterIntake(double value) {
     _waterIntake = value;
     notifyListeners();
+    saveData(); // Сохраняем данные при каждом изменении
   }
 
   void addWaterIntake(double value) {
     _waterIntake += value;
+    notifyListeners();
+    saveData(); // Сохраняем данные при каждом изменении
+  }
+
+  Future<void> saveData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('selectedGender', _selectedGender);
+    prefs.setInt('age', _age);
+    prefs.setDouble('weight', _weight);
+    prefs.setDouble('climate', _climate);
+    prefs.setDouble('waterIntake', _waterIntake);
+  }
+
+  Future<void> loadData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _selectedGender = prefs.getInt('selectedGender') ?? 0;
+    _age = prefs.getInt('age') ?? 0;
+    _weight = prefs.getDouble('weight') ?? 0.0;
+    _climate = prefs.getDouble('climate') ?? 1.0;
+    _waterIntake = prefs.getDouble('waterIntake') ?? 0.0;
     notifyListeners();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:water_friend/pages/questionnaire.dart';
+import 'package:water_friend/pages/questionnaire.dart'; // Подключите ваш GenderScreen
+import 'package:water_friend/pages/home_screen.dart'; // Подключите ваш home_screen
 import 'package:provider/provider.dart';
 import 'functions/user_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  UserProvider userProvider = UserProvider();
+  await userProvider.loadData();
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => UserProvider()),
+        ChangeNotifierProvider.value(value: userProvider),
       ],
       child: MyApp(),
     ),
@@ -18,7 +23,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GenderScreen(),
+      home: FutureBuilder(
+        // Используем FutureBuilder для проверки состояния приложения
+        future: checkFirstTime(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // Если выполнение завершено, проверяем результат
+            if (snapshot.data == true) {
+              // Если это первый раз, покажем GenderScreen
+              return GenderScreen();
+            } else {
+              // В противном случае, покажем home_screen
+              return HomeScreen();
+            }
+          } else {
+            // Пока идет проверка, можно показать какой-то индикатор загрузки
+            return HomeScreen();
+          }
+        },
+      ),
     );
+  }
+
+  Future<bool> checkFirstTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool firstTime = prefs.getBool('first_time') ?? true;
+
+    if (firstTime) {
+      // Если это первый раз, установим значение в false и вернем true
+      prefs.setBool('first_time', false);
+      return true;
+    } else {
+      // Если это не первый раз, вернем false
+      return false;
+    }
   }
 }
