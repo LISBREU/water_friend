6d9e130b18846244195521361c4f7df4da2af39f
Ilnur Shabanov <ilnurshabanov79@gmail.com>
2024-01-01 21:29:30 +0300

    reupdate to working version

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,46 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import '../functions/notifications.dart';
 
-class Settings extends StatefulWidget {
-  @override
-  _SettingsState createState() => _SettingsState();
-}
-
-class _SettingsState extends State<Settings> {
-  @override
-  void initState() {
-    super.initState();
-    LocalNotification.initilaze(flutterLocalNotificationsPlugin);
-  }
-
+class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(20)),
-          width: 180,
-          height: 50,
-          child: ElevatedButton(
-            onPressed: () {
-              LocalNotification.startAutomaticNotifications(
-                  flutterLocalNotificationsPlugin:
-                      flutterLocalNotificationsPlugin);
-            },
-            child: const Text(
-              "send",
-              style: TextStyle(
-                fontSize: 18,
-              ),
-            ),
-          ),
+    return Center(
+      child: Text(
+        'Settings',
+        style: TextStyle(
+          color: Colors.blue,
         ),
       ),
     );
   }
 }
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
